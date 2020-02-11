29875d9b4b283c98a5e4353e9004d6cf7d39686f
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-02-11 16:20:35 +0900

    ✨🙈🐛 pet api 생성, user정보 보여주는 api 생성 pet api 추가 user 정보 보여주는 api 추가 gitinore 주석 삭제 walk/list에서 fakeData 보여주는 에러 수정 walk/list에서 모든 날짜의 list가 보여주는 에러 수 user/edit의 에러 고침

---
+++ walk/src/controllers/petsControllers.ts
@@ -0,0 +1,73 @@
+import { Request, Response } from 'express';
+import { getRepository } from "typeorm";
+import env from 'dotenv';
+import jwt from "jsonwebtoken";
+import { Users } from '../entity/Users';
+import { Pets } from '../entity/Pets';
+
+env.config();
+
+export const PetCreate = async function (req: Request, res: Response) {
+    const UserId = jwt.verify(req.headers.authorization, process.env.KEY).id;
+    if (UserId) {
+        const PetMaster = await getRepository(Users).findOne({
+            where: {
+                id: UserId
+            }
+        });
+        // tslint:disable-next-line: await-promise
+        const Pet = await getRepository(Pets).create({
+            name: req.body.name,
+            age: req.body.age,
+            image: req.body.image,
+            gender: req.body.gender,
+            keyword: req.body.keyword,
+            user: PetMaster
+        });
+        // tslint:disable-next-line: no-floating-promises
+        getRepository(Pets).save(Pet).then(data => {
+            // console.log("data", data);
+            if (data) {
+                res.sendStatus(201);
+            } else {
+                res.sendStatus(400);
+            }
+        });
+    } else {
+        res.status(403);
+        res.json({
+            error: {
+                status: 403,
+                type: "ExpiredToken",
+                message: "만료된 토큰입니다. 비밀번호 재설정 요청을 다시 해주세요."
+            }
+        });
+    }
+};
+
+export const GetUserPets = async function (req: Request, res: Response) {
+    const UserId = jwt.verify(req.headers.authorization, process.env.KEY).id;
+    if (UserId) {
+        const PetList = await getRepository(Pets).find({
+            where: {
+                user: UserId
+            }
+        });
+        if (PetList.length > 0) {
+            res.status(200);
+            res.json(PetList);
+        } else {
+            res.status(200);
+            res.json([]);
+        }
+    } else {
+        res.status(403);
+        res.json({
+            error: {
+                status: 403,
+                type: "ExpiredToken",
+                message: "만료된 토큰입니다. 비밀번호 재설정 요청을 다시 해주세요."
+            }
+        });
+    }
+};
--- walk/src/routes/UserRoutes.ts
+++ walk/src/routes/UserRoutes.ts
@@ -1,7 +1,8 @@
 import express, { Router } from 'express';
-import { signup, login, edit } from '../controllers/usercontollers';
+import { signup, login, edit, Info } from '../controllers/usercontollers';
 const router: Router = express.Router();
 
+router.get('/', Info);
 router.post('/signup', signup);
 router.post('/login', login);
 router.patch('/edit', edit);
