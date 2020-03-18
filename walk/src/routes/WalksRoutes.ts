e5d475b5fe5162797c778954df7a46e1725218eb
윤승현 <yunseunghyeon@yunseunghyeon-ui-MacBookPro.local>
2020-03-18 17:58:55 +0900

    🙈✨ walk/edit api 추가 walk/edit 추가 gitinore 파일 수정

--- walk/src/routes/WalksRoutes.ts
+++ walk/src/routes/WalksRoutes.ts
@@ -1,10 +1,11 @@
 import express, { Router } from 'express';
-import { list, detail, create } from '../controllers/walkscontrollers';
+import { list, detail, create, edit } from '../controllers/walkscontrollers';
 const router: Router = express.Router();
 
 router.get('/list', list);
 router.get('/detail', detail);
 router.post('/create', create);
+router.patch('/edit', edit);
 
 
 export default router; 
